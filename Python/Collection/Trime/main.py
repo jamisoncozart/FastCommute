b98845724af798fdb05babeda081de0515776af0
jraypdx <44383003+jraypdx@users.noreply.github.com>
2019-10-17 21:46:09 -0700

    Fixed script and added more comments

--- Python/Collection/Trime/main.py
+++ Python/Collection/Trime/main.py
@@ -4,27 +4,33 @@ import runRoute
 import os
 
 numRuns = 1440 #Number of iterations in the main loop.  1440 minutes in a day
-
-
+links = []
+threads = []
 
 # waits until fresh minute to start the script
-timeToStart = 60 - int(time.strftime('%S'))
+timeToStart = 60 - int(time.strftime('%S')) #finds out how many seconds until the next minute
 print("Starting in " + str(timeToStart) + " seconds.")
-while (time.strftime('%S') != "00"):
+while (time.strftime('%S') != "00"): #sleeps the program for 1 second until the next minute is reached
     time.sleep(1)
 
-links = []
+#Reads in routes.txt line for line, and splits each line into the name and link
 with open ("C:/Devtop/Routes/Routes.txt") as f:
     for line in f:
         links.append(line.split())
 
+#goes through each route that was added, starts it on a new thread by calling newRoute.newRoute(...)
+#then wait 5 seconds before starting the next
 for route in links:
-    th = threading.Thread(target=runRoute.runRoute, args=(route[0], route[1], numRuns, time.strftime('%S')))
-    th.start()
+    threads.append(threading.Thread(target=runRoute.runRoute, args=(route[0], route[1], numRuns, time.strftime('%S'))))
+    threads[len(threads)-1].start()
     #runRoute.runRoute(route[0], route[1], numRuns)
     time.sleep(5)
 
-th.join()
-th.join()
+#Waits for all of the threads to finish to continue (Will happen at 11:59 pm)
+for thread in threads:
+    thread.join()
 
+#restarts the comp to start fresh for the next day, but gives the user 10 sec in case they want to stop it for debugging
+print ("\n\nCollection complete\nSYSTEM WILL NOW SHUT DOWN")
+time.sleep(10)
 os.system("shutdown -t 0 -r -f")
--- Python/Collection/Trime/runRoute.py
+++ Python/Collection/Trime/runRoute.py
@@ -6,34 +6,43 @@ import time
 import re
 import json
 
+#Starts a new chrome page and runs a route based off of passed in parameters
 def runRoute(routeName, routeLink, numRuns, offset):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    driver = webdriver.Chrome(chrome_options=options)
+    #start the chrome page
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--ignore-certificate-errors')
+    #options.add_argument('--ignore-ssl-errors')
+    driver = webdriver.Chrome()#chrome_options=options)
     driver.set_window_size(50,50)
     driver.minimize_window()
     driver.get(routeLink)
+
+    #set the first element of the data output array to the current time and the link for the route
     data = [[time.strftime('%Y:%m:%d'), routeLink]]
-    fileOutput = "C:/Devtop/Routes/" + routeName + "/" + time.strftime('%Y-%m-%d-%H-%M') + " "# + str(numIt)
+    #Set the path to save the file later
+    fileOutput = "C:/Devtop/Routes/" + routeName + "/" + time.strftime('%Y-%m-%d-%H-%M') + " "
     print(routeName + " beginning data collection for " + time.strftime('%A'))
 
-    #put a bigger 1-24h loop around this one that takes time.time() at beginning and reloops every 24*60*60 seconds
+    #Runs a loop every minute numRuns of times (1440 = 24 hours) or until 11:59 pm
     for a in range(0,numRuns):
+        #break if it's the end of the day
         if (time.strftime('%H:%M') == "23:59"):
             break
-        if (a % 180 == 0 and a != 0): #Prints to command prompt every 30 minutes to let me know it's still running
+        if (a % 60 == 0 and a != 0): #Prints to command prompt every hour to let me know it's still running
             print ("script is running " + time.strftime('%H:%M:%S'))
 
+        #Refresh the chrome page and wait 5 sec to make sure it has loaded again
         driver.refresh()
         sleep(5)
 
+        #Get all of the code for the page after it's loaded and find the travel time from it
         src = driver.page_source
         tTime = re.findall(r'.(id=\"section-directions-trip-0\")?>(([\d]+) h ([\d]+) min|([\d]+) min)', src)
         sleep(1)
         tTime = tTime[0][1]
         #print (tTime)
 
+        #caclulate the number of minutes in the route from the travel time string pulled from the page code
         totalMin = 0
         if (" h " in tTime):
             totalMin = int(tTime[0]) * 60 + int(tTime[4:6])
@@ -42,15 +51,17 @@ def runRoute(routeName, routeLink, numRuns, offset):
         else: #if something weird happens like a road is closed or a multi day route is picked no data is collected
             totalMin = 0
 
+        #Add the time that was found (in minutes) to the data output array, and wait until the next minute
         data.append([time.strftime('%H:%M:%S'), totalMin])
-        #print (str(totalMin) + " minutes.")
-        #print(time.strftime('%S'))
         while (time.strftime('%S') != offset):
             sleep(1)
 
+    #Oncce the loop has finished, close chrome and add the time and amount of minutes ran to the data ouput array
     driver.close()
     data.append([time.strftime('%Y:%m:%d'), str(len(data)-2)])
 
+    #Add the number of times the loop ran (number of minutes the program collected data) to data ouput array
+    #And save the file to the fileOutput created earlier
     fileOutput += str(len(data) - 2 )
     #print("Saving file to: " + fileOutput)
     with open(fileOutput, 'w') as f:
